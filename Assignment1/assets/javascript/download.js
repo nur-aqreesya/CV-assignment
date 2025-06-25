9849253cf89e53e196820b8e5628d9a028d03f40
Nur Aqreesya <nur_24007567@utp.edu.my>
2025-06-26 01:13:57 +0800

    Update download.js

--- Assignment1/assets/javascript/download.js
+++ Assignment1/assets/javascript/download.js
@@ -15,14 +15,21 @@ document.getElementById("downloadCV").addEventListener("click", function () {
   });
 
   Promise.all(imageLoadPromises).then(() => {
-    const opt = {
-  margin: [0.5, 0.5], 
+  const opt = {
+  margin: [0.3, 0.3], // tighter margin
   filename: "esya-cv.pdf",
   image: { type: "jpeg", quality: 0.98 },
-  html2canvas: { scale: 2, useCORS: true },
+  html2canvas: {
+    scale: 2,
+    useCORS: true,
+    scrollY: 0,
+    scrollX: 0,
+    windowWidth: 1200 // forces consistent width snapshot
+  },
   jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
   pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
 };
+
 
     html2pdf().set(opt).from(cvElement).save().then(() => {
       cvElement.style.display = "none"; // Hide after download

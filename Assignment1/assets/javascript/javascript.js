5bf107a31b7e8a37e25c0a01199b4379fcacf53d
Nur Aqreesya <nur_24007567@utp.edu.my>
2025-06-26 00:04:36 +0800

    Delete Assignment1 directory

--- Assignment1/assets/javascript/javascript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleBtn = document.getElementById('darkModeToggle');
-
-  toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    document.querySelector('.header-top').classList.toggle('dark-header');
-  });
-});
